286333948a3ef27f9c7c543f5fe936df92fa564c
carlosnp <machinery.e@gmail.com>
2019-01-05 08:30:26 -0400

    se crearon dos botones para ordenar alfabeticamente los proyectos, el primero segun el titulo y el segundo segun el nombre del la persona

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,6 +4,19 @@
   <h1 class="subheading grey--text">Dashboard Page</h1>
 
   <v-container fluid class="my-5">
+
+    <v-layout row wrap class="mb-3">
+      <v-btn small flat color="grey" @click="sortBy('title')">
+        <v-icon left small>folder</v-icon>
+        <span class="caption text-lowercase"> Nombre del Proyecto </span>
+      </v-btn>
+
+      <v-btn small flat color="grey" @click="sortBy('person')">
+        <v-icon left small>person</v-icon>
+        <span class="caption text-lowercase"> Persona </span>
+      </v-btn>
+    </v-layout>
+
     <!-- card 1 -->
     <v-card flat class="pa-1" v-for="project in projects" :key="project.title">
       <v-layout row wrap :class="`pa-3 project ${project.status}`">
@@ -47,6 +60,11 @@ export default {
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    sortBy(prop){
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
     }
   }
 }
