4376a6d043ee96fa285f43c086c364d7fb53fe31
carlosnp <machinery.e@gmail.com>
2019-01-05 09:17:52 -0400

    se agregaron las imagenes a cada mienbro del temaplate team

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -7,7 +7,10 @@
 			<v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
 				<v-card flat class="text-xs-center ma-3">
 					<v-responsive class="pt-4">
-						imagen
+						<!-- Imagen -->
+						<v-avatar size="100" class="grey lighten-2">
+							<img :src="person.avatar">
+						</v-avatar>
 					</v-responsive>
 					<!-- Card con el nombre y el rol de cada persona -->
 					<v-card-text>
@@ -37,11 +40,11 @@ export default {
 	data() {
 		return {
 			team: [
-				{ name: 'The Net Ninja', role: 'Web developer' },
-				{ name: 'Ryu', role: 'Graphic designer' },
-				{ name: 'Chun Li', role: 'Web developer' },
-				{ name: 'Gouken', role: 'Social media maverick' },
-				{ name: 'Yoshi', role: 'Sales guru'}
+				{ name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png' },
+				{ name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+				{ name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+				{ name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+				{ name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
 			]
 		}
 	}
