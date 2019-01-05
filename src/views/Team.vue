0ae6adebdd7a4c99a47f6c67ddfa0116b9f77bdc
carlosnp <machinery.e@gmail.com>
2019-01-05 09:04:20 -0400

    creacion del template team.vue

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,11 +1,30 @@
 <template>
 <div class="team">
-  <h1 class="subheading grey--text">Team Page</h1>
-
-  <v-container fluid class="my-5">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quasi voluptates sequi unde, ab laboriosam deleniti earum porro placeat tempora quae error necessitatibus ipsam at dolorum veniam dolores, doloremque possimus!</p>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum cum, nobis odio excepturi, perspiciatis quod modi voluptates nesciunt eaque possimus suscipit consequuntur delectus. Quod assumenda doloribus voluptatum impedit nam nemo!</p>
-  </v-container>
+	<h1 class="subheading grey--text">Team Page</h1>
+	<v-container fluid class="my-5">
+		<!-- Fila de elementos -->
+		<v-layout row wrap>
+			<v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+				<v-card flat class="text-xs-center ma-3">
+					<v-responsive class="pt-4">
+						imagen
+					</v-responsive>
+					<!-- Card con el nombre y el rol de cada persona -->
+					<v-card-text>
+						<div class="subheading">{{ person.name }}</div>
+						<div class="grey--text">{{ person.role }}</div>
+					</v-card-text>
+					<!-- Card para botones -->
+					<v-card-actions>
+						<v-btn flat color="grey">
+							<v-icon small left>message</v-icon>
+							<span>Mensaje</span>
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
 
 
 </div>
@@ -15,5 +34,16 @@
   
 
 export default {
+	data() {
+		return {
+			team: [
+				{ name: 'The Net Ninja', role: 'Web developer' },
+				{ name: 'Ryu', role: 'Graphic designer' },
+				{ name: 'Chun Li', role: 'Web developer' },
+				{ name: 'Gouken', role: 'Social media maverick' },
+				{ name: 'Yoshi', role: 'Sales guru'}
+			]
+		}
+	}
 }
 </script>
